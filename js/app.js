6b2e1d555a4e049e1fcc296e0492f9182c2d602f
shohan89 <shohansharoar89@gmail.com>
2022-09-03 01:09:11 +0600

    dynamic categories created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log( 'Connected!!!' );
+//TODO: Load Category Data
+const loadData = () => {
+  fetch( 'https://openapi.programming-hero.com/api/news/categories' )
+    .then(res => res.json())
+    .then( data => displayCategories(data.data) );
+    
+}
+
+//TODO: Display the loaded category to the category section
+const displayCategories = categories => {
+  
+  let catArray = categories.news_category //* This is showing a array of all categories.
+  
+  const btnContainer = document.getElementById( 'btn-container' )
+  catArray.forEach(category => {
+    const {category_name, category_id} = category;
+    
+    const btnDiv = document.createElement('div');
+    btnDiv.classList.add( 'col' );
+    btnDiv.innerHTML = `
+      <button class="btn">${category_name}</button>
+    `;
+    btnContainer.appendChild(btnDiv);
+  });
+} 
+
+
+loadData();
